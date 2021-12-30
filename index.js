4dd1855a8c029e8d42f761946036b04bc06d7ae5
Visne <39844191+Visne@users.noreply.github.com>
2021-12-30 17:25:25 +0100

    Add base support

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const config = require("./config.json");
-
+const path = require("path")
 const express = require("express");
 const app = express();
 
-app.use(express.static("public"));
+let base = "/";
+if (config.hasOwnProperty("base")) {
+    base += config.base;
+}
+
+app.use(base, express.static(path.join(__dirname, "public")));
 
 const port = config.port;
 app.listen(port, () => console.log("Listening on http://localhost:" + port));
