6c1e37cbdd2d243828b3872b416c67f1f46c7343
Visne <39844191+Visne@users.noreply.github.com>
2021-12-31 06:33:23 +0100

    Add handlebars

--- index.js
+++ index.js
@@ -1,14 +1,38 @@
 const config = require("./config.json");
-const path = require("path")
-const express = require("express");
-const app = express();
+const Path = require("path")
+const Express = require("express");
+const ExpressHandlebars = require("express-handlebars");
 
+const app = Express();
+
+// Set up rendering engine
+app.engine('.hbs', ExpressHandlebars.engine({
+    extname: ".hbs",
+    defaultLayout: "index"
+}));
+app.set('view engine', '.hbs');
+app.set('views', Path.join(__dirname, 'views'));
+
+// Set base url, if specified
 let base = "/";
 if (config.hasOwnProperty("base")) {
     base += config.base;
 }
 
-app.use(base, express.static(path.join(__dirname, "public")));
+// Host index.hbs with main.hbs view
+app.get(base, (req, res) => {
+    res.render("main", {
+        clientId: config.clientId,
+        redirectUri: config.redirectUri
+    });
+})
 
+// Check if port is specified in config
+if (!config.hasOwnProperty("port")) {
+    console.log("No port specified in config.json!");
+    process.exit(1);
+}
+
+// Listen on port
 const port = config.port;
 app.listen(port, () => console.log("Listening on http://localhost:" + port));
